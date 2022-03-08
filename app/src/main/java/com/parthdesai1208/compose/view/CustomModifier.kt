9c4f076452a6fdf03a11cc3ca7b37655d2c90270
parth_neosoft <parth.desai@neosoftmail.com>
2022-03-08 18:03:41 +0530

    Add custom layout for displaying list of text in column

--- app/src/main/java/com/parthdesai1208/compose/view/CustomModifier.kt
+++ app/src/main/java/com/parthdesai1208/compose/view/CustomModifier.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -35,7 +36,7 @@ fun Modifier.baseLineToTop(firstBaselineToTop: Dp): Modifier {
     )
 }
 
-@Preview
+
 @Composable
 fun TextWithPaddingToBaselinePreview() {
     ComposeTheme {
@@ -43,10 +44,54 @@ fun TextWithPaddingToBaselinePreview() {
     }
 }
 
-@Preview
+
 @Composable
 fun TextWithNormalPaddingPreview() {
     ComposeTheme {
         Text("Hi there!", Modifier.padding(top = 32.dp))
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // Don't constrain child views further, measure them with given constraints
+        // List of measured children
+        val placeables = measurables.map { measurable ->
+            // Measure each child
+            measurable.measure(constraints)
+        }
+
+        // Track the y co-ord we have placed children up to
+        var yPosition = 0
+
+        // Set the size of the layout as big as it can
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // Place children in the parent layout
+            placeables.forEach { placeable ->
+                // Position item on the screen
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                // Record the y co-ord placed up to
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
     }
 }
