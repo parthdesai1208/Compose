6802d041dde9a31efc8112266e497c19457b449d
parthdesai1208 <dparth78@gmail.com>
2022-08-17 21:23:38 +0530

    Navigate to BottomSheet using compose destination lib.

--- app/src/main/java/com/parthdesai1208/compose/view/navigation/composeDestination/ComposeDestinationLibraryExample.kt
+++ app/src/main/java/com/parthdesai1208/compose/view/navigation/composeDestination/ComposeDestinationLibraryExample.kt
@@ -1,25 +1,53 @@
 package com.parthdesai1208.compose.view.navigation.composeDestination
 
 import android.os.Parcelable
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.navigation.plusAssign
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.google.accompanist.navigation.material.rememberBottomSheetNavigator
+import com.parthdesai1208.compose.view.navigation.composeDestination.destinations.BottomSheetUsingComposeDestinationDestination
 import com.parthdesai1208.compose.view.navigation.composeDestination.destinations.DialogComposeDestination
 import com.parthdesai1208.compose.view.navigation.composeDestination.destinations.SecondScreenDestination
 import com.parthdesai1208.compose.view.navigation.composeDestination.destinations.ThirdScreenDestination
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.spec.DestinationStyle
 import kotlinx.parcelize.Parcelize
 
+@OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialNavigationApi::class)
 @Composable
 fun StartForComposeDestination() {
-    DestinationsNavHost(navGraph = NavGraphs.root)
+    //for without bottom sheet
+//    DestinationsNavHost(navGraph = NavGraphs.root)
+
+    //for bottom sheet you have to set things like below otherwise simply write 'DestinationsNavHost'
+    val navController = rememberAnimatedNavController()
+    val bottomSheetNavigator = rememberBottomSheetNavigator()
+    navController.navigatorProvider += bottomSheetNavigator
+    com.google.accompanist.navigation.material.ModalBottomSheetLayout(
+        bottomSheetNavigator = bottomSheetNavigator,
+        sheetBackgroundColor = Color.Transparent
+    ) {
+        DestinationsNavHost(
+            navGraph = NavGraphs.root,
+            navController = navController,
+            engine = rememberAnimatedNavHostEngine()
+        )
+    }
+
 }
 
 @RootNavGraph(start = true)
@@ -47,6 +75,9 @@ fun FirstScreen(navigator: DestinationsNavigator) {
             }
             Button(onClick = { navigator.navigate(direction = DialogComposeDestination) }) {
                 Text(text = "Open Dialog")
+            }
+            Button(onClick = { navigator.navigate(direction = BottomSheetUsingComposeDestinationDestination) }) {
+                Text(text = "Open BottomSheet")
             }
         }
     }
@@ -109,6 +140,34 @@ fun DialogCompose(navigator: DestinationsNavigator) {
                     .padding(8.dp)
             ) {
                 Text(text = "OK")
+            }
+        }
+    }
+}
+
+@Destination(style = DestinationStyle.BottomSheet::class)
+@Composable
+fun BottomSheetUsingComposeDestination(navigator: DestinationsNavigator) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        shape = RoundedCornerShape(32.dp)
+    ) {
+        Column {
+            Spacer(modifier = Modifier.height(28.dp))
+            Text(
+                text = "You are just open 'bottom sheet' using compose destination library",
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.padding(horizontal = 24.dp)
+            )
+            TextButton(
+                onClick = { navigator.popBackStack() },
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(8.dp)
+            ) {
+                Text(text = "Fine")
             }
         }
     }
