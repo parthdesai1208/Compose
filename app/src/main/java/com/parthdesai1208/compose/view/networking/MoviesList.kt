cf1c1daac7093a06fa3aad73f0d9332be4976e02
parthdesai1208 <dparth78@gmail.com>
2023-01-10 18:56:39 +0530

    make card selectable in MoviesList.kt

--- app/src/main/java/com/parthdesai1208/compose/view/networking/MoviesList.kt
+++ app/src/main/java/com/parthdesai1208/compose/view/networking/MoviesList.kt
@@ -3,6 +3,7 @@ package com.parthdesai1208.compose.view.networking
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -13,6 +14,7 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -115,21 +117,47 @@ fun MoviesLoadingScreen(isLoading: Boolean) {
 
 @Composable
 fun MoviesList(moviesList: List<MoviesList>) {
+    val selectedItemList = remember { mutableStateListOf<Int>() }
+
     LazyColumn(content = {
-        itemsIndexed(items = moviesList) { _, item ->
-            MovieItem(movie = item)
+        itemsIndexed(items = moviesList) { index, item ->
+            MovieItem(index, movie = item, selectedList = selectedItemList, onItemClick = {
+                    if (!selectedItemList.contains(it)) {
+                        selectedItemList.add(it)
+                    } else {
+                        selectedItemList.removeAt(selectedItemList.indexOf(it))
+                    }
+            })
         }
     })
 }
 
 @Composable
-fun MovieItem(movie: MoviesList) {
+fun MovieItem(
+    index: Int,
+    movie: MoviesList,
+    selectedList: SnapshotStateList<Int>,
+    onItemClick: (Int) -> Unit
+) {
     val context = LocalContext.current
+
+    val backgroundColor = if (selectedList.contains(index)) {
+        MaterialTheme.colors.primary
+    } else {
+        MaterialTheme.colors.background
+    }
+
     Card(
         modifier = Modifier
             .padding(horizontal = 8.dp, vertical = 4.dp)
             .fillMaxWidth()
-            .height(120.dp), shape = RoundedCornerShape(8.dp), elevation = 4.dp
+            .height(120.dp)
+            .clickable {
+                onItemClick(index)
+            },
+        shape = RoundedCornerShape(8.dp),
+        elevation = 4.dp,
+        backgroundColor = backgroundColor
     ) {
         Row(
             modifier = Modifier
@@ -194,7 +222,11 @@ fun MoviesItemPreview() {
     ComposeTheme {
         Surface {
             moviesPreviewList.take(1).forEach {
-                MovieItem(movie = it)
+                MovieItem(
+                    index = 0,
+                    movie = it,
+                    selectedList = emptyList<Int>() as SnapshotStateList<Int>,
+                    onItemClick = {})
             }
         }
     }
