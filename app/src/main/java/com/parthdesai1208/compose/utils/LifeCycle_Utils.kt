549bc7eb3963c9a32da34060cc972c3e44586429
parthdesai1208 <dparth78@gmail.com>
2023-02-13 21:36:31 +0530

    add ObserveLifeCycle compose in LifeCycle_Utils.kt

---
+++ app/src/main/java/com/parthdesai1208/compose/utils/LifeCycle_Utils.kt
@@ -0,0 +1,38 @@
+package com.parthdesai1208.compose.utils
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+
+@Composable
+fun ObserveLifeCycle(
+    onCreate: () -> Unit = {},
+    onStart: () -> Unit = {},
+    onResume: () -> Unit = {},
+    onPause: () -> Unit = {},
+    onStop: () -> Unit = {},
+    onDestroy: () -> Unit = {},
+    onAny: () -> Unit = {},
+) {
+    val owner = LocalLifecycleOwner.current
+
+    DisposableEffect(key1 = owner, effect = {
+        val observer = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_CREATE -> onCreate()
+                Lifecycle.Event.ON_START -> onStart()
+                Lifecycle.Event.ON_RESUME -> onResume()
+                Lifecycle.Event.ON_PAUSE -> onPause()
+                Lifecycle.Event.ON_STOP -> onStop()
+                Lifecycle.Event.ON_DESTROY -> onDestroy()
+                else -> onAny()
+            }
+        }
+        owner.lifecycle.addObserver(observer)
+        onDispose {
+            owner.lifecycle.removeObserver(observer)
+        }
+    })
+}
