755d16625905ef534d1f6b53988739ecdaa5fcc0
parthdesai1208 <dparth78@gmail.com>
2024-07-06 20:02:59 +0530

    add new navigation in ConstraintLayout.kt

--- app/src/main/java/com/parthdesai1208/compose/view/uicomponents/ConstraintLayout.kt
+++ app/src/main/java/com/parthdesai1208/compose/view/uicomponents/ConstraintLayout.kt
@@ -16,48 +16,55 @@ import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.parthdesai1208.compose.utils.AddBackIconToScreen
 import com.parthdesai1208.compose.view.theme.ComposeTheme
 
 @Composable
-fun ConstraintLayoutContent() {
-    Surface {
-        ConstraintLayout(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-        ) {
+fun ConstraintLayoutContent(navHostController: NavHostController) {
+    AddBackIconToScreen(screen = {
+        Surface {
+            ConstraintLayout(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            ) {
 
-            val (button1, text, button2) = createRefs()
-            createHorizontalChain(button1, button2, chainStyle = ChainStyle.Spread)
-            Button(onClick = { },
-                modifier = Modifier.constrainAs(button1) {
-                    top.linkTo(parent.top, 16.dp) //for single link
-                    linkTo(parent.start, button2.start)
-                }) {
-                Text(text = "Button1")
+                val (button1, text, button2) = createRefs()
+                createHorizontalChain(button1, button2, chainStyle = ChainStyle.Spread)
+                Button(onClick = { },
+                    modifier = Modifier.constrainAs(button1) {
+                        top.linkTo(parent.top, 16.dp) //for single link
+                        linkTo(parent.start, button2.start)
+                    }) {
+                    Text(text = "Button1")
+                }
+
+                Button(onClick = { },
+                    modifier = Modifier.constrainAs(button2) {
+                        top.linkTo(button1.top) //for single link
+                        bottom.linkTo(button1.bottom)
+                        linkTo(button1.end, parent.end)
+                    }) {
+                    Text(text = "Button2")
+                }
+
+                Text(text = "Text",
+                    modifier = Modifier.constrainAs(text) {
+                        linkTo(
+                            button1.bottom,
+                            parent.bottom,
+                            10.dp,
+                            8.dp,
+                            bias = 0f
+                        ) //for vertical link
+                        linkTo(button1.end, button1.end) //for single link
+                    })
             }
-
-            Button(onClick = { },
-                modifier = Modifier.constrainAs(button2) {
-                    top.linkTo(button1.top) //for single link
-                    bottom.linkTo(button1.bottom)
-                    linkTo(button1.end, parent.end)
-                }) {
-                Text(text = "Button2")
-            }
-
-            Text(text = "Text",
-                modifier = Modifier.constrainAs(text) {
-                    linkTo(
-                        button1.bottom,
-                        parent.bottom,
-                        10.dp,
-                        8.dp,
-                        bias = 0f
-                    ) //for vertical link
-                    linkTo(button1.end, button1.end) //for single link
-                })
         }
+    }) {
+        navHostController.popBackStack()
     }
 }
 
@@ -66,31 +73,35 @@ fun ConstraintLayoutContent() {
 fun ConstraintLayoutContentPreview() {
     ComposeTheme {
         //ConstraintLayoutContent()
-        DecoupledConstraintLayout()
+        DecoupledConstraintLayout(rememberNavController())
     }
 }
 
 @Composable
-fun DecoupledConstraintLayout() {
-    Surface {
-        BoxWithConstraints {
-            val constraints = if (maxWidth < maxHeight) {
-                decoupledConstraints(margin = 16.dp) // Portrait constraints
-            } else {
-                decoupledConstraints(margin = 64.dp) // Landscape constraints
-            }
-
-            ConstraintLayout(constraints) {
-                Button(
-                    onClick = { /* Do something */ },
-                    modifier = Modifier.layoutId("button")
-                ) {
-                    Text("Button")
+fun DecoupledConstraintLayout(navHostController: NavHostController) {
+    AddBackIconToScreen(screen = {
+        Surface {
+            BoxWithConstraints {
+                val constraints = if (maxWidth < maxHeight) {
+                    decoupledConstraints(margin = 16.dp) // Portrait constraints
+                } else {
+                    decoupledConstraints(margin = 64.dp) // Landscape constraints
                 }
 
-                Text("Text", Modifier.layoutId("text"))
+                ConstraintLayout(constraints) {
+                    Button(
+                        onClick = { /* Do something */ },
+                        modifier = Modifier.layoutId("button")
+                    ) {
+                        Text("Button")
+                    }
+
+                    Text("Text", Modifier.layoutId("text"))
+                }
             }
         }
+    }) {
+        navHostController.popBackStack()
     }
 }
 
@@ -109,50 +120,54 @@ private fun decoupledConstraints(margin: Dp): ConstraintSet {
 }
 
 @Composable
-fun ClockByConstraintLayout() {
-    Surface {
-        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-            val (middleDot, one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve) = createRefs()
-            Text(text = "Clock", modifier = Modifier.constrainAs(middleDot) {
-                linkTo(start = parent.start, end = parent.end)
-                linkTo(top = parent.top, bottom = parent.bottom)
-            })
-            Text(text = "1", modifier = Modifier.constrainAs(one) {
-                circular(other = middleDot, angle = 30f, distance = 90.dp)
-            })
-            Text(text = "2", modifier = Modifier.constrainAs(two) {
-                circular(other = middleDot, angle = 60f, distance = 90.dp)
-            })
-            Text(text = "3", modifier = Modifier.constrainAs(three) {
-                circular(other = middleDot, angle = 90f, distance = 90.dp)
-            })
-            Text(text = "4", modifier = Modifier.constrainAs(four) {
-                circular(other = middleDot, angle = 120f, distance = 90.dp)
-            })
-            Text(text = "5", modifier = Modifier.constrainAs(five) {
-                circular(other = middleDot, angle = 150f, distance = 90.dp)
-            })
-            Text(text = "6", modifier = Modifier.constrainAs(six) {
-                circular(other = middleDot, angle = 180f, distance = 90.dp)
-            })
-            Text(text = "7", modifier = Modifier.constrainAs(seven) {
-                circular(other = middleDot, angle = 210f, distance = 90.dp)
-            })
-            Text(text = "8", modifier = Modifier.constrainAs(eight) {
-                circular(other = middleDot, angle = 240f, distance = 90.dp)
-            })
-            Text(text = "9", modifier = Modifier.constrainAs(nine) {
-                circular(other = middleDot, angle = 270f, distance = 90.dp)
-            })
-            Text(text = "10", modifier = Modifier.constrainAs(ten) {
-                circular(other = middleDot, angle = 300f, distance = 90.dp)
-            })
-            Text(text = "11", modifier = Modifier.constrainAs(eleven) {
-                circular(other = middleDot, angle = 330f, distance = 90.dp)
-            })
-            Text(text = "12", modifier = Modifier.constrainAs(twelve) {
-                circular(other = middleDot, angle = 0f, distance = 90.dp)
-            })
+fun ClockByConstraintLayout(navHostController: NavHostController) {
+    AddBackIconToScreen(screen = {
+        Surface {
+            ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+                val (middleDot, one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve) = createRefs()
+                Text(text = "Clock", modifier = Modifier.constrainAs(middleDot) {
+                    linkTo(start = parent.start, end = parent.end)
+                    linkTo(top = parent.top, bottom = parent.bottom)
+                })
+                Text(text = "1", modifier = Modifier.constrainAs(one) {
+                    circular(other = middleDot, angle = 30f, distance = 90.dp)
+                })
+                Text(text = "2", modifier = Modifier.constrainAs(two) {
+                    circular(other = middleDot, angle = 60f, distance = 90.dp)
+                })
+                Text(text = "3", modifier = Modifier.constrainAs(three) {
+                    circular(other = middleDot, angle = 90f, distance = 90.dp)
+                })
+                Text(text = "4", modifier = Modifier.constrainAs(four) {
+                    circular(other = middleDot, angle = 120f, distance = 90.dp)
+                })
+                Text(text = "5", modifier = Modifier.constrainAs(five) {
+                    circular(other = middleDot, angle = 150f, distance = 90.dp)
+                })
+                Text(text = "6", modifier = Modifier.constrainAs(six) {
+                    circular(other = middleDot, angle = 180f, distance = 90.dp)
+                })
+                Text(text = "7", modifier = Modifier.constrainAs(seven) {
+                    circular(other = middleDot, angle = 210f, distance = 90.dp)
+                })
+                Text(text = "8", modifier = Modifier.constrainAs(eight) {
+                    circular(other = middleDot, angle = 240f, distance = 90.dp)
+                })
+                Text(text = "9", modifier = Modifier.constrainAs(nine) {
+                    circular(other = middleDot, angle = 270f, distance = 90.dp)
+                })
+                Text(text = "10", modifier = Modifier.constrainAs(ten) {
+                    circular(other = middleDot, angle = 300f, distance = 90.dp)
+                })
+                Text(text = "11", modifier = Modifier.constrainAs(eleven) {
+                    circular(other = middleDot, angle = 330f, distance = 90.dp)
+                })
+                Text(text = "12", modifier = Modifier.constrainAs(twelve) {
+                    circular(other = middleDot, angle = 0f, distance = 90.dp)
+                })
+            }
         }
+    }) {
+        navHostController.popBackStack()
     }
 }
