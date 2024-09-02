00e65d005916ba7368287ce1d02352dbb952f7ea
parthdesai1208 <dparth78@gmail.com>
2024-09-02 19:33:02 +0530

    add missing imports in HorizontalPagerCompose.kt

--- app/src/main/java/com/parthdesai1208/compose/view/uicomponents/HorizontalPagerCompose.kt
+++ app/src/main/java/com/parthdesai1208/compose/view/uicomponents/HorizontalPagerCompose.kt
@@ -10,8 +10,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
-import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
